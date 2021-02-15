a5aaa8bee5339f130f71761d47dd29377806cc3a
Liam Tengelis <liam@tengelisconsulting.com>
2021-02-15 13:52:55 -0800

    state utils

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from "./@types/http-types";
 export { HttpService } from "./service/http-service";
 export { AuthState } from "./state/auth-state";
 export { HttpState } from "./state/http-state";
+
+export { makeState } from "./util/state";
--- src/util/state.ts
+++ src/util/state.ts
@@ -7,10 +7,41 @@ export function makeState<S>(
   const impl = {
     state: initial,
   };
+  const updateCbs: {
+    [index: string]: (update: Partial<S>) => void,
+  } = {};
   return {
+
+    // return the state
     get: () => impl.state,
+
+    // update the state, which also triggers
+    // any update listeners to fire
     update: (stateUpdate: Partial<S>): void => {
+      Object.values(updateCbs).forEach((updateFn) => {
+        updateFn(stateUpdate);
+      });
       impl.state = shallowMerge(impl.state, stateUpdate);
     },
+
+    // add a listener for state updates
+    addUpdateCb: (
+      filter: (update: Partial<S>) => boolean,
+      cb: (update: Partial<S>) => void,
+    ): string => {
+      const id = Date.now().toString();
+      updateCbs[id] = (update: Partial<S>) => {
+        if (filter(update)) {
+          cb(update);
+        }
+      };
+      return id;
+    },
+
+    // remove a listener for state updates
+    clearUpdateCb: (id: string): void => {
+      delete updateCbs[id];
+    },
+
   };
 }
